0132a81816530aa13b387d2dc1b4b97328deace3
Dmitry <admin@statichun.ru>
2022-01-29 20:46:20 +0700

    Update KinOnline.user.js
    A: Binding to top of the poster.
    F: The button is not displayed if the movie is available by subscription.

--- KinOnline.user.js
+++ KinOnline.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KinOnline
 // @namespace    kinonline
-// @version      0.1
+// @version      0.2
 // @description  Watch on Kinopoisk for free!
 // @author       StaticHUN
 // @match        *://www.kinopoisk.ru/*/*
@@ -9,26 +9,34 @@
 // ==/UserScript==
 
 function init() {
-  const type = window.location.href.split('/')[3];
-  if (type === 'film' || type === 'series') {
-    const id = window.location.href.split('/')[4];
-    if (id) {
-      const url = new URL('https://statichun.ru/videoPlayer.html');
-      url.searchParams.set('id', id);
+    let art = document.getElementsByClassName('styles_watchOnlineButtonPurchased__2_U4z');
+    if (art.length == 0) {
+        let element = document.querySelector(`div[data-tid="fe27f3c4"]`);//fe27f3c4 - poster; cc89b13d - trailer
+        if (element.length != 0) {
+            let coords = element.getBoundingClientRect();
 
-      const button = document.createElement('div');
-      button.id = 'btn';
-      button.innerHTML = `<style>.button {font-family: "Graphik Kinopoisk LC Web",Arial,Tahoma,Verdana,sans-serif; font-size: 100%; font-size: 15px; color: #fff; background: #f60; cursor: pointer; padding: 8px 115.5px; position: absolute; border: none; border-radius: 6px;} .button:hover {background-color: #f25900}</style><center><button class="button" title="Cмотреть">Смотреть</button></center>`;
-      button.style.position = 'absolute';
-      button.style.zIndex = '1';
-      button.style.width = '100%';
-      button.style.top = '153px';
-      button.style.left = '-590px';
-      button.style.minWidth = '1280px';
-      button.addEventListener('click', () => window.open(url.toString(), '_blank').focus());
-      document.body.appendChild(button);
+            const type = window.location.href.split('/')[3];
+            if (type == 'film' || type == 'series') {
+                const id = window.location.href.split('/')[4];
+                if (id) {
+                    const url = new URL('https://statichun.ru/videoPlayer.html');
+                    url.searchParams.set('id', id);
+
+                    const button = document.createElement('div');
+                    button.id = 'btn';
+                    button.innerHTML = `<style>.button {font-family: "Graphik Kinopoisk LC Web",Arial,Tahoma,Verdana,sans-serif; font-size: 100%; font-size: 15px; font-weight: 500; color: #fff; background: #f60; cursor: pointer; padding: 8px 86px; position: absolute; border: none; border-radius: 3px;} .button:hover {background-color: #f25900}</style><center><button class="button" title="Cмотреть">Смотреть онлайн</button></center>`;
+                    button.style.position = 'absolute';
+                    button.style.zIndex = '1';
+                    button.style.width = '100%';
+                    button.style.top = coords.top - 1 + 'px';
+                    button.style.left = '-590px';
+                    button.style.minWidth = '1200px';
+                    button.addEventListener('click', () => window.open(url.toString(), '_blank').focus());
+                    document.body.appendChild(button);
+                }
+            }
+        }
     }
-  }
 }
 
 window.addEventListener('load', init);
